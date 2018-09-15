88b8233442cc8ca7812e6fcd6249ce38b08a1970
summersab <arthur.summers@gmail.com>
2018-09-15 15:08:14 -0500

    Pretty big overhaul
    This adds a lot of changes because my previous versions didn't have functional calls for the track and identify calls. Here's a mostly-complete list of changes:
    - Removed the guzzle requirement by borrowing from Order Desk's API helper client: https://gist.github.com/sparkweb/c6a5a21ab44a23589b9c
    - Instantiation now allows for providing the api_key, token, or both. It also allows providing a version number for the Lists v2 API (all other calls are made to the v1 endpoint)
    - Added numerous error checks to ensure that the api_key, token, and version number are properly set. I PROBABLY shouldn't throw exceptions to do this, but it seemed like the right thing to do at 1:00 AM.
    - The track and identify calls are technically GET requests, so they can be called in this manner.

--- src/Klaviyo.php
+++ src/Klaviyo.php
@@ -1,72 +1,127 @@
 <?php
 
-namespace Klaviyo;
-
-use GuzzleHttp\Client;
-
 class KlaviyoException extends Exception { }
 
 class Klaviyo
 {
     protected $api_key;
-    protected $client;
-    public function __construct($api_key, Client $client = null)
-    {
-        $this->api_key = $api_key;
-        if (!is_null($client)) {
-            $this->client = $client;
-        } else {
-            $this->client = new Client(array(
-                'base_uri' => 'https://a.klaviyo.com/api/v1/'
-            ));
+    protected $token;
+    protected $version;
+    protected $base_url = "https://a.klaviyo.com/api/";
+    protected $TRACK_ONCE_KEY = '__track_once__';
+    public $last_status_code = "";
+
+    public function __construct($api_key = null, $token = null, $version = 1) {
+        if ($api_key != null && strpos($api_key, "pk_") === 0 && strlen($api_key) == 37) {
+            $this->api_key = $api_key;
+        }
+        else if ($api_key != null) {
+            throw new KlaviyoException("Invalid API key. Key should begin with pk_ followed by a 34 character hexidecimal string. See https://help.klaviyo.com/hc/en-us/articles/115005062267-Manage-Your-Account-s-API-Keys for more details.");
+        }
+
+        if ($token != null && strlen($token) == 6) {
+            $this->token = $token;
+        }
+        else if ($token != null) {
+            throw new KlaviyoException("Invalid token. Token should be a six-character, capitalized, alphanumeric string. See https://help.klaviyo.com/hc/en-us/articles/115005062267-Manage-Your-Account-s-API-Keys for more details.");
+        }
+        if ($version !== null && is_numeric($version) && $version >= 1) {
+            $this->version = $version;
+        }
+        else if ($version !== null) {
+            throw new KlaviyoException("Invalid version number. Version must be an integer greater than zero.");
         }
     }
-    public function getApiKey()
-    {
-        return $this->api_key;
+
+    public function get($url = "", $params = null) {
+        return $this->go("GET", $url, $params);
     }
-    public function request($method, $path, array $options = array())
-    {
-        $response = $this->client->request($method, $path, $options);
-        $body = json_decode($response->getBody()->getContents(), true);
-        return $body;
+
+    public function post($url, $params = null) {
+        return $this->go("POST", $url, $params);
     }
-    public function get($path, array $params = array())
-    {
+
+    public function put($url, $params = null) {
+        return $this->go("PUT", $url, $params);
+    }
+
+    public function delete($url, $params = null) {
+        return $this->go("DELETE", $url, $params);
+    }
+
+    public function go($method, $url, $params) {
+        //The track and identify calls are TECHNICALLY GET requests. This allows them to be called in that manner.
+        if ($method == "GET" && strpos($url, "track_once") === 0) {
+            if (!isset($params['properties'])) $params['properties'] = null;
+            if (!isset($params['time'])) $params['time'] = null;
+            return $this->track_once($params['event'], $params['customer_properties'], $params['properties'], $params['time']);
+        }
+        if ($method == "GET" && strpos($url, "track") === 0) {
+            if (!isset($params['properties'])) $params['properties'] = null;
+            if (!isset($params['time'])) $params['time'] = null;
+            return $this->track_once($params['event'], $params['customer_properties'], $params['properties'], $params['time']);
+        }
+        if ($method == "GET" && strpos($url, "identify") === 0) {
+            return $this->identify($params);
+        }
+
+        if (!is_array($params)) {
+            $params = null;
+        }
+        if (!$url) {
+            throw new KlaviyoException("Please enter a destination url");
+        }
+        if ($this->api_key == null) {
+            throw new KlaviyoException("You must specify an API key for this request");
+        }
+
+        //Version Override for Lists API
+        if (strpos($url, "list") === 0) {
+            $url =  $this->base_url . "v" . $this->version . "/" . $url;
+        }
+        else {
+            $url =  $this->base_url . "v1/" . $url;
+        }
+
         $params['api_key'] = $this->api_key;
-        return $this->request('GET', $path, array(
-            'query' => $params
-        ));
-    }
-    public function post($path, array $params = array())
-    {
-        $params['api_key'] = $this->api_key;
-        return $this->request('POST', $path, array(
-            'form_params' => $params
-        ));
-    }
-    public function put($path, array $params = array())
-    {
-        $params['api_key'] = $this->api_key;
-        return $this->request('PUT', $path, array(
-            'form_params' => $params
-        ));
-    }
-    public function delete($path, array $params = array())
-    {
-        $params['api_key'] = $this->api_key;
-        return $this->request('DELETE', $path, array(
-            'form_params' => $params
-        ));
+
+        //GET and PUT Override
+        if (($method == "GET" || $method == "PUT") && $params !== null) {
+            $url .= (strpos($url, "?") === false ? "?" : "") . http_build_query($params);
+            $params = "";
+        }
+
+        //Setup cURL
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        if ($params) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, urlencode(base64_encode(json_encode($params))));
+        }
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+
+        //Send To Klaviyo and Parse Response
+        $response = trim(curl_exec($ch));
+        $info = curl_getinfo($ch);
+        $json = json_decode($response, 1);
+
+        if (!is_array($json)) {
+            return $response;
+        }
+
+        $this->last_status_code = $info['http_code'];
+        return $json;
     }
     public function track($event, $customer_properties=array(), $properties=array(), $timestamp=NULL) {
         if ((!array_key_exists('$email', $customer_properties) || empty($customer_properties['$email']))
             && (!array_key_exists('$id', $customer_properties) || empty($customer_properties['$id']))) {
-            
+
             throw new KlaviyoException('You must identify a user by email or ID.');
         }
         $params = array(
-            'token' => $this->api_key,
             'event' => $event,
             'properties' => $properties,
             'customer_properties' => $customer_properties
@@ -75,7 +130,7 @@ class Klaviyo
             $params['time'] = $timestamp;
         }
         $encoded_params = $this->build_params($params);
-        return $this->make_request('api/track', $encoded_params);
+        return $this->make_request('track', $encoded_params);
     }
     public function track_once($event, $customer_properties=array(), $properties=array(), $timestamp=NULL) {
         $properties[$this->TRACK_ONCE_KEY] = true;
@@ -84,21 +139,25 @@ class Klaviyo
     public function identify($properties) {
         if ((!array_key_exists('$email', $properties) || empty($properties['$email']))
             && (!array_key_exists('$id', $properties) || empty($properties['$id']))) {
-            
+
             throw new KlaviyoException('You must identify a user by email or ID.');
         }
         $params = array(
-            'token' => $this->api_key,
             'properties' => $properties
         );
         $encoded_params = $this->build_params($params);
-        return $this->make_request('api/identify', $encoded_params);
+        return $this->make_request('identify', $encoded_params);
     }
     protected function build_params($params) {
+        if ($this->token == null) {
+            throw new KlaviyoException("You must specify a token key for this request");
+        }
+
+        $params['token'] = $this->token;
         return 'data=' . urlencode(base64_encode(json_encode($params)));
     }
     protected function make_request($path, $params) {
-        $url = $this->host . $path . '?' . $params;
+        $url = $this->base_url . $path . '?' . $params;
         $response = file_get_contents($url);
         return $response == '1';
     }
